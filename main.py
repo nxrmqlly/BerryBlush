c5c5d14330bbb69476ce486e26c6f0798fca9e6f
NxrmqL <86097735+Shadowed-codes@users.noreply.github.com>
2022-06-04 19:33:45 +0530

    update logging

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ from discord.ext import commands, tasks
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='./logs/discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 load_dotenv()
